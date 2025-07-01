01816a6278857f5cae8c63d39dc360ab485c0425
Kerry McCullough <subone@subone.org>
2025-06-30 21:08:42 -0600

    Selection, validation, and styles

--- js/component/practice-terms.js
+++ js/component/practice-terms.js
@@ -5,47 +5,107 @@ import { practiceTermsEl } from '../elements.js';
 import { practice } from '../practice.js';
 import PracticeTermsRowComponent from './practice-terms/row.js';
 import PracticeTermComponent from './practice-terms/row/term.js';
-import { terms } from '../terms.js';
 
 export default Component.define
 
 `<div class="terms terms-list"></div>`
 
 (class extends Component {
+  termsListElement;
+  termElements;
+  matchElements;
+  emptyTermElements;
+  emptyMatchElements;
+  selectedElements;
+  elementTermMap;
+  elementMatchMap;
+
   constructor(element) {
     super(element);
-
-    const termListEl = select('.terms-list', element);
-
+    this.termsListElement = select('.terms-list', element);
     practice.on('start', () => {
-      termListEl.innerText = '';
-      const roundCount = 6;
-      const termCount = 5;
-      const count = roundCount * termCount;
-
-      // Pick random term pairs
-      const randomTerms = terms.getRandomByCount(count);
-      const randomMatches = [];
-      for (let i = 0; i < count; ++i) {
-        randomMatches.push(randomTerms[i].match);
-      }
-
-      // Mix up matches
-      shuffle(randomMatches, 0, 5);
-
-      // Render term elements
-      for (let i = 0; i < termCount; ++i) {
-        const termRowCmp = PracticeTermsRowComponent();
-        const termRowEl = termRowCmp.element;
-        const termCmp = PracticeTermComponent();
-        const termEl = termCmp.element;
-        termEl.innerText = randomTerms[i].term;
-        const matchCmp = PracticeTermComponent();
-        const matchEl = matchCmp.element;
-        matchEl.innerText = randomMatches[i];
-        termRowEl.append(termEl, matchEl);
-        practiceTermsEl.append(termRowEl);
-      }
+      this.renderInitialTermElements();
+      this.fillTerms();
     });
+  }
+
+  renderInitialTermElements() {
+    this.selectedElements = [ null, null ];
+    this.termElements = [];
+    this.matchElements = [];
+    this.emptyTermElements = [];
+    this.emptyMatchElements = [];
+    this.elementTermMap = new WeakMap();
+    this.elementMatchMap = new WeakMap();
+
+    this.termsListElement.innerText = '';
+
+    const { termCount } = practice;
+    for (let i = 0; i < termCount; ++i) {
+      const termRowCmp = PracticeTermsRowComponent();
+      const termRowEl = termRowCmp.element;
+
+      const termCmp = PracticeTermComponent();
+      const termEl = termCmp.element;
+      termEl.addEventListener('click', () => this.selectElement(0, termEl));
+      this.termElements.push(termEl);
+      this.emptyTermElements.push(termEl);
+
+      const matchCmp = PracticeTermComponent();
+      const matchEl = matchCmp.element;
+      matchEl.addEventListener('click', () => this.selectElement(1, matchEl));
+      this.matchElements.push(matchEl);
+      this.emptyMatchElements.push(matchEl);
+
+      termRowEl.append(termEl, matchEl);
+      practiceTermsEl.append(termRowEl);
+    }
+  }
+
+  fillTerms() {
+    const count = this.emptyMatchElements.length;
+    if (!count) {
+      return;
+    }
+    shuffle(this.emptyMatchElements);
+    const terms = practice.consumeTerms(count);
+    const {
+      emptyTermElements, emptyMatchElements,
+      elementTermMap, elementMatchMap,
+    } = this;
+    for (let i = 0; i < terms.length; ++i) {
+      const term = terms[i];
+      const termEl = emptyTermElements.shift();
+      termEl.innerText = term.term;
+      elementTermMap.set(termEl, term);
+      const matchEl = emptyMatchElements.shift();
+      matchEl.innerText = term.match;
+      elementMatchMap.set(matchEl, term);
+    }
+  }
+
+  selectElement(selectionIndex, element) {
+    if (this.selectedElements[selectionIndex]) {
+      this.selectedElements[selectionIndex].classList.remove('selected', 'incorrect');
+    }
+    this.selectedElements[selectionIndex] = element;
+    element.classList.add('selected');
+    this.checkForTermMatch();
+  }
+
+  checkForTermMatch() {
+    const [ termEl, matchEl ] = this.selectedElements;
+    if (!( termEl && matchEl )) {
+      return;
+    }
+    const term = this.elementTermMap.get(termEl);
+    const isMatch = term === this.elementMatchMap.get(matchEl);
+    const oldClass = isMatch ? 'incorrect' : 'correct';
+    const newClass = isMatch ? 'correct' : 'incorrect';
+    termEl.classList.remove(oldClass, 'selected');
+    termEl.classList.add(newClass);
+    matchEl.classList.remove(oldClass, 'selected');
+    matchEl.classList.add(newClass);
+    this.selectedElements = [ null, null ];
   }
 });
--- js/practice.js
+++ js/practice.js
@@ -1,4 +1,5 @@
 import { Events } from './events.js';
+import { terms } from './terms.js';
 
 export const practice = new class PracticeService {
   events = new Events();
@@ -11,7 +12,27 @@ export const practice = new class PracticeService {
     return this.events.off(event, callback);
   }
 
+  roundCount = 6;
+  termCount = 5;
+  correctFillDelay = 500;
+  incorrectSelectionDelay = 0;
+  nextTermIndex = 0;
+  terms = [];
+
+  get count() {
+    return (this.roundCount ?? 0) * (this.termCount ?? 0);
+  }
+
   start() {
+    this.nextTermIndex = 0;
+    this.terms = terms.getRandomByCount(this.count)
+      .map((term, index) => ({ ...term, index }));
     this.events.emitWithContext(this, 'start');
+  }
+
+  consumeTerms(count) {
+    const { nextTermIndex } = this;
+    this.nextTermIndex += count;
+    return this.terms.slice(nextTermIndex, this.nextTermIndex);
   }
 };
